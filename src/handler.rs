65633118fe22dbaf3329522edb50d3722c9ea31a
elcharitas <jonathanirhodia@gmail.com>
2023-03-25 23:37:22 +0100

    fix: handle undefined params

--- src/handler.rs
+++ src/handler.rs
@@ -4,10 +4,26 @@ use crate::crawl_and_search;
 use crate::enums::SearchRequest;
 
 pub async fn search_handler(request: Request) -> Result<Response<Body>, Error> {
-    assert!(request.method() == "POST");
-    assert!(request.headers().get("Content-Type").unwrap() == "application/json");
-    assert!(&request.body().is_empty());
-    let search_request: SearchRequest = serde_json::from_slice(&request.body())?;
+    let body = String::from_utf8(request.body().to_vec())?;
+
+    if body.is_empty() {
+        return Ok(Response::builder()
+            .status(StatusCode::NOT_ACCEPTABLE)
+            .header("Content-Type", "application/json")
+            .body(Body::from("Please specify query"))
+            .unwrap());
+    }
+
+    // extract params from body and convert to SearchRequest
+    let search_request: SearchRequest = serde_json::from_str(&body).unwrap();
+
+    if search_request.query.is_empty() || search_request.sitemap_url.is_empty() {
+        return Ok(Response::builder()
+            .status(StatusCode::NOT_ACCEPTABLE)
+            .header("Content-Type", "application/json")
+            .body(Body::from("Please specify query"))
+            .unwrap());
+    }
 
     let search_results = crawl_and_search(&search_request).await;
     Ok(Response::builder()
