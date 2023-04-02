fa6a4acfea9802062f0e804a9aeb9bc431acee87
elcharitas <jonathanirhodia@gmail.com>
2023-04-02 18:22:55 +0100

    fix: snippet extraction

--- src/extraction.rs
+++ src/extraction.rs
@@ -1,6 +1,6 @@
 use select::document::Document;
 use select::node::Node;
-use select::predicate::{Attr, Name, Predicate};
+use select::predicate::Any;
 
 use crate::enums::SearchRequest;
 
@@ -35,23 +35,19 @@ pub fn extract_request(query: SearchRequest) -> SearchRequest {
 // This function extracts a relevant text snippet from the given HTML content and search query
 pub fn extract_snippet(html_content: &str, search_query: &str) -> Option<String> {
     let document = Document::from(html_content);
-    let search_query_lower = search_query.to_lowercase();
 
     // Define a custom predicate to match text nodes containing the search query
-    let text_contains_query = |node: &Node| {
+    let node_has_query = |node: &Node| {
         if let Some(text) = node.as_text() {
-            text.to_lowercase().contains(&search_query_lower)
+            text.to_lowercase().contains(&search_query.to_lowercase())
         } else {
             false
         }
     };
 
-    // Find the first matching text node and extract the snippet
-    if let Some(text_node) = document
-        .find(Attr("class", "content").descendant(Name("p").child(text_contains_query)))
-        .next()
-    {
-        let snippet = text_node.text().trim().to_string();
+    // Find the first matching text node where search_query is found and extract the snippet
+    if let Some(text_node) = document.find(Any).filter(node_has_query).next() {
+        let snippet = text_node.as_text()?.trim().to_string();
         Some(snippet)
     } else {
         None
